bfe4d0b3cbc2d1852260b68402bd2dfd574cb02d
solana <solana.liu@alumni.emory.edu>
2020-09-02 13:36:37 -0700

    fixed typo

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-app.use((err, req, res, next) => {
+App.use((err, req, res, next) => {
   res.status(500).json({ type: "error", message: err.message });
 });
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
 ReactDOM.render(<App />, document.getElementById("root"));
