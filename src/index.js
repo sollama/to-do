9a2370c7fadeabd2accee02e5fb0012ad34f82b7
solana <solana.liu@alumni.emory.edu>
2020-09-02 13:46:08 -0700

    cleaned up files

--- src/index.js
+++ src/index.js
@@ -2,11 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-App.use((err, req, res, next) => {
-  res.status(500).json({ type: "error", message: err.message });
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
-
 ReactDOM.render(<App />, document.getElementById("root"));
